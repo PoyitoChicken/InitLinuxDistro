8cf4018a00aad1fb8154eb84cc1863140d451263
Sylvia Cortés <82062415+PoyitoChicken@users.noreply.github.com>
2024-03-06 08:26:39 -0600

    untested fix issue #4

--- installer_4fedora.sh
+++ installer_4fedora.sh
@@ -2,7 +2,15 @@ echo "#####****     Updating dependencies       ****####"
 sudo dnf update -y
 
 echo "#####****     Installing Programming Dependencies...      ****####"
-sudo dnf install -y community-mysql-server dbeaver-code- vscode postgresql gcc gcc-c++ nodejs npm python3 git docker kubernetes-cli dotnet-sdk-5.0 java-latest-openjdk-devel
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/F-35-x86_64/pgdg-fedora-repo-latest.noarch.rpm
+sudo dnf update -y
+sudo dnf install -y community-mysql-server dbeaver code postgresql gcc gcc-c++ nodejs npm python3 git docker kubernetes-cli dotnet java-latest-openjdk-devel postgresql11-server postgresql11 heimdal-devel
+
 
 echo "Making MariaDB run at start"
 sudo systemctl start mysqld
